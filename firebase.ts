700edb5c0c72f244e18453380085f108f9d5718b
ralphiesmarketing113 <rtj113@gmail.com>
2024-10-18 14:05:59 -0700

    Updaing firebase env variables

--- firebase.ts
+++ firebase.ts
@@ -1,14 +1,18 @@
 import { getStorage } from "firebase/storage";
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+declare var process: {
+  env: {
+    [x: string]: string
+    NODE_ENV: string
+  }
+}
 const firebaseConfig = {
-  apiKey: "AIzaSyArCVIMmHoFLQt2Q_2XlripY7soVYL4FBA",
-  authDomain: "ai-to-pdf-file-chat.firebaseapp.com",
-  projectId: "ai-to-pdf-file-chat",
-  storageBucket: "ai-to-pdf-file-chat.appspot.com",
-  messagingSenderId: "934533732178",
-  appId: "1:934533732178:web:bff7cc4ec386d802b34c8b"
+  apiKey: process.env.FIREBASE_PRIVATE_KEY,
+  authDomain: process.env.FIREBASE_AUTH_URI,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.FIREBASE_CLIENT_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
